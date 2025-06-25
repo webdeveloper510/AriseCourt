c35c9fe926d7760940f35bb366975e62a6b033ba
webdeveloper510 <developerweb6@gmail.com>
2025-06-25 10:55:12 +0530

    updated code with formula

--- user/utils.py
+++ user/utils.py
@@ -3,14 +3,14 @@ from datetime import datetime
 
 def calculate_total_fee(court, duration_hours):
     base_fee = float(court.court_fee_hrs) * duration_hours
-    total = base_fee / 0.80
-    tax = total * 0.10
-    cc_fee = total * 0.10
+    tax = base_fee * 0.10
+    cc_fee = base_fee * 0.10
+    total = base_fee + tax + cc_fee
     return {
-        "total_amount": int(total * 100),
         "base_fee": base_fee,
         "tax": tax,
-        "cc_fee": cc_fee
+        "cc_fee": cc_fee,
+        "total_amount": int(total * 100)
     }
 
 
