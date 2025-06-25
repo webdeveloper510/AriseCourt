d1c0c84be091972bb19f52b2610408d02b912619
webdeveloper510 <developerweb6@gmail.com>
2025-06-25 13:12:26 +0530

    add database

--- user/models.py
+++ user/models.py
@@ -12,8 +12,7 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True,null=True)
     updated_at = models.DateTimeField(auto_now=True,null=True)
 
-    class Meta:
-        abstract = True
+    
 
 
 
