677930d7178ab26c7d2cb166d304320be61668d5
webdeveloper510 <developerweb6@gmail.com>
2025-07-23 17:23:49 +0530

    updated code

--- user/serializers.py
+++ user/serializers.py
@@ -464,13 +464,15 @@ class CourtDataSerializer(serializers.ModelSerializer):
 class CourtBookingWithUserSerializer(serializers.ModelSerializer):
     user = UsersDataSerializer(read_only=True)
     court = CourtDataSerializer(read_only=True)
+    tax = serializers.SerializerMethodField()
+    cc_fees = serializers.SerializerMethodField()
 
     class Meta:
         model = CourtBooking
         fields = [
             'id', 'booking_date', 'start_time', 'end_time', 'duration_time',
             'total_price', 'on_amount', 'book_for_four_weeks', 'status',
-            'created_at', 'updated_at', 'user', 'court'
+            'created_at', 'updated_at', 'user', 'court','tax','cc_fees'
         ]
 
     def get_tax(self, obj):
@@ -486,7 +488,7 @@ class CourtBookingWithUserSerializer(serializers.ModelSerializer):
             cc = Decimal(str(obj.total_price)) * Decimal(str(obj.court.cc_fees)) / Decimal('100')
             return cc.quantize(Decimal('0.01'), rounding=ROUND_DOWN)
         return Decimal('0.00')
-    
+
 
 
 
