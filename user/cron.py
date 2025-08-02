041d05dfcea8af4000d2c88d51f66b0a00f9c16a
webdeveloper510 <developerweb6@gmail.com>
2025-08-02 12:54:27 +0530

    change in 4 weeks booking

--- user/cron.py
+++ user/cron.py
@@ -2,7 +2,7 @@ import requests
 
 def deletePendingBookings():
     try:
-        delete_url = 'https://get1court.com/delete_pending_bookings/'
+        delete_url = 'https://api.get1court.com/delete_pending_bookings/'
         response = requests.delete(delete_url)
         response.raise_for_status()
         print("Bookings Deleted Successfully.")
